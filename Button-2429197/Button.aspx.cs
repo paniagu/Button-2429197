23ebb35d0b9c1514e95e95b65786c7508e101b9f
Paniagu <paniaguadavid084@gmail.com>
2025-01-31 20:26:39 -0600

    Agregar archivos de proyecto.

---
+++ Button-2429197/Button.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Button_2429197
+{
+    public partial class Button : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
